1282d9f07c4df6f80259e462606d83ce18eb6e3a
LopesRM <rafhael.m.lopes.cadastro@gmail.com>
2025-03-28 20:41:18 -0300

    13 commit

--- script.js
+++ script.js
@@ -3,3 +3,20 @@
     height: auto; /* Mantém a proporção */
     border-radius: 10px; /* Opcional: bordas arredondadas */
 }
+
+// Seleciona o título animado
+const tituloAnimado = document.getElementById('titulo-animado');
+
+// Inicializa o contador de cliques
+let clickCount = 0;
+
+// Adiciona um evento de clique ao título
+tituloAnimado.addEventListener('click', () => {
+    clickCount++; // Incrementa o contador
+
+    // Verifica se o contador atingiu 7 cliques
+    if (clickCount === 7) {
+        // Redireciona para o link do YouTube
+        window.location.href = 'https://www.youtube.com/watch?v=6RiIww6jVXw';
+    }
+});
